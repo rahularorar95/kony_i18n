c19b6120bef0e10d3b40b0cd5da74d98161f6766
Rahul Arora <rahularrar95@gmail.com>
2018-08-23 00:28:10 +0530

    added py files

---
+++ prop2xl/prop2xl.py
@@ -0,0 +1,22 @@
+import xlwt
+from xlwt import Workbook
+i18nFile= open("i18n.properties")
+row = 1
+wb = Workbook()
+sheet = wb.add_sheet('Sheet1')
+style = xlwt.easyxf('font: bold 1')
+for _ in i18nFile:
+    arr = _.strip()
+    if(arr[0]=="#"):
+        continue
+    length = 0
+    key_value = [arr[:arr.find("=")].rstrip()] + arr[arr.find("=")+1:].lstrip().split("~|~")
+    value = key_value[:len(key_value)-1]
+    if(key_value[0]=="Key"):
+        for i in range(len(value)):
+            sheet.write(0,i,value[i],style)
+    for i in range(len(value)):
+        sheet.write(row,i,value[i])
+    row = row+1
+wb.save("i18n.xls")
+i18nFile.close()
---
+++ xl2prop/xl2prop.py
@@ -0,0 +1,16 @@
+import xlrd
+import datetime
+wb = xlrd.open_workbook("i18n.xls")
+sheet = wb.sheet_by_index(0)
+file = open("i18n.properties", "w")
+date = datetime.datetime.now().strftime("%c")
+file.write("# "+date[:-14]+''+date[-5:]+date[:-5][-9:]+" GMT+0530 (India Standard Time)\n")
+for row in range(1,sheet.nrows):
+    for col in range(0,sheet.ncols):
+        if(col == 0):
+            file.write(sheet.cell_value(row,col)+"=")
+        else:
+            file.write(sheet.cell_value(row,col)+"~|~")
+    file.write("\n")
+file.close()
+
